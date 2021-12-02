9107e28f03de692838d8ea5949b08577e635d72b
louromero <82890300+louromero@users.noreply.github.com>
2021-12-01 21:39:55 -0300

    Add files via upload

---
+++ Espejo-Romero/java.js
@@ -0,0 +1,84 @@
+(function() {
+    // Variables
+    var $curve = document.getElementById("curve");
+    var last_known_scroll_position = 0;
+    var defaultCurveValue = 350;
+    var curveRate = 3;
+    var ticking = false;
+    var curveValue;
+  
+    // Handle the functionality
+    function scrollEvent(scrollPos) {
+      if (scrollPos >= 0 && scrollPos < defaultCurveValue) {
+        curveValue = defaultCurveValue - parseFloat(scrollPos / curveRate);
+        $curve.setAttribute(
+          "d",
+          "M 800 300 Q 400 " + curveValue + " 0 300 L 0 0 L 800 0 L 800 300 Z"
+        );
+      }
+    }
+  
+    // Scroll Listener
+    // https://developer.mozilla.org/en-US/docs/Web/Events/scroll
+    window.addEventListener("scroll", function(e) {
+      last_known_scroll_position = window.scrollY;
+  
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          scrollEvent(last_known_scroll_position);
+          ticking = false;
+        });
+      }
+  
+      ticking = true;
+    });
+  })();
+  
+
+/*--------------------------CARRUSEL--------------------------*/
+
+/*--------------------------PRODUCTOS----------------------------------*/
+$(function () {
+  $(".material-card > .mc-btn-action").click(function () {
+    var card = $(this).parent(".material-card");
+    var icon = $(this).children("i");
+    icon.addClass("fa-spin-fast");
+
+    if (card.hasClass("mc-active")) {
+      card.removeClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-arrow-left")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-bars");
+      }, 800);
+    } else {
+      card.addClass("mc-active");
+
+      window.setTimeout(function () {
+        icon
+          .removeClass("fa-bars")
+          .removeClass("fa-spin-fast")
+          .addClass("fa-arrow-left");
+      }, 800);
+    }
+  });
+});
+
+
+/*----------------------------------------------------------------------MAPA------------------------------------------------------------------------------------------------*/
+function initMap() {
+  var myLatLng = {lat: 40.7127837, lng: -74.00594130000002};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 16
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng
+  });
+}
